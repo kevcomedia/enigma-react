b15c839d3b3a652061a586611b79c42144275a99
Kev Comedia <kevcomedia@gmail.com>
2018-08-22 19:58:40 +0800

    Extract letters from Keyboard to its own module
    The lampboard has its lamps arranged in the same way, so I factored it
    out for the lampboard to use.

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Keyboard.css';
 
 import Key from './Key';
+import letterRows from './letter-rows';
 
 class Keyboard extends Component {
   state = {
@@ -25,15 +26,9 @@ class Keyboard extends Component {
   };
 
   render() {
-    const keyRows = [
-      ['Q', 'W', 'E', 'R', 'T', 'Z', 'U', 'I', 'O'],
-      ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K'],
-      ['P', 'Y', 'X', 'C', 'V', 'B', 'N', 'M', 'L'],
-    ];
-
     return (
       <div className="Keyboard">
-        {keyRows.map((row, idx) => (
+        {letterRows.map((row, idx) => (
           <div className="Keyboard__row" key={idx}>
             {row.map((key) => (
               <Key
