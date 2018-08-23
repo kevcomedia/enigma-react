aefea7641cc02f67c678f9f1dd9544d8ff7670c0
Kev Comedia <kevcomedia@gmail.com>
2018-08-23 12:27:53 +0800

    Remove own letter from Plug choices
    A Plug can't connect to itself, so its own letter shouldn't appear as an
    option.

--- src/Plugboard.js
+++ src/Plugboard.js
@@ -3,19 +3,27 @@ import './Plugboard.css';
 import Plug from './Plug';
 import letterRows from './letter-rows';
 
-class Plugboard extends Component {
-  render() {
-    const alphabet = Array.from({ length: 26 }).map((_, i) =>
-      String.fromCharCode(i + 65),
-    );
+const alphabet = Array.from({ length: 26 }).map((_, i) =>
+  String.fromCharCode(i + 65),
+);
 
+class Plugboard extends Component {
+  renderPlug = (letter) => {
+    return (
+      <Plug
+        letter={letter}
+        availableLetters={alphabet.filter((l) => l !== letter)}
+        key={letter}
+      />
+    );
+  };
+
+  render() {
     return (
       <div className="Plugboard">
         {letterRows.map((row, idx) => (
           <div className="Plugboard__row" key={idx}>
-            {row.map((letter) => (
-              <Plug letter={letter} availableLetters={alphabet} key={letter} />
-            ))}
+            {row.map(this.renderPlug)}
           </div>
         ))}
       </div>
